7420cfb60cb45c62c32c1f468cef1641527423ce
ajlopez <webmaster@ajlopez.com>
2013-04-20 17:55:37 -0300

    Vacuum Perception List tests, prepare for Table Driven

---
+++ Src/SharpAmia.Tests/Agents/VacuumPerceptionListTests.cs
@@ -0,0 +1,36 @@
+﻿namespace SharpAmia.Tests.Agents
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpAmia.Agents;
+
+    [TestClass]
+    public class VacuumPerceptionListTests
+    {
+        [TestMethod]
+        public void Equals()
+        {
+            VacuumPerceptionList l1 = new VacuumPerceptionList(new VacuumPerception(VacuumLocation.A, VacuumStatus.Clean));
+            VacuumPerceptionList l2 = new VacuumPerceptionList(new VacuumPerception(VacuumLocation.A, VacuumStatus.Clean), new VacuumPerception(VacuumLocation.B, VacuumStatus.Clean));
+            VacuumPerceptionList l3 = new VacuumPerceptionList(new VacuumPerception(VacuumLocation.A, VacuumStatus.Clean), new VacuumPerception(VacuumLocation.B, VacuumStatus.Dirty));
+            VacuumPerceptionList l4 = new VacuumPerceptionList(new VacuumPerception(VacuumLocation.A, VacuumStatus.Dirty));
+            VacuumPerceptionList l5 = new VacuumPerceptionList(new VacuumPerception(VacuumLocation.A, VacuumStatus.Clean));
+
+            Assert.IsTrue(l1.Equals(l5));
+            Assert.IsTrue(l5.Equals(l1));
+            Assert.AreEqual(l1.GetHashCode(), l5.GetHashCode());
+
+            Assert.IsFalse(l1.Equals(null));
+            Assert.IsFalse(l1.Equals(123));
+            Assert.IsFalse(l1.Equals(l2));
+            Assert.IsFalse(l2.Equals(l1));
+            Assert.IsFalse(l1.Equals(l3));
+            Assert.IsFalse(l3.Equals(l1));
+            Assert.IsFalse(l1.Equals(l4));
+            Assert.IsFalse(l4.Equals(l1));
+        }
+    }
+}
---
+++ Src/SharpAmia/Agents/VacuumPerceptionList.cs
@@ -0,0 +1,55 @@
+﻿namespace SharpAmia.Agents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class VacuumPerceptionList
+    {
+        private static int hashcode = typeof(VacuumPerceptionList).GetHashCode();
+
+        private IList<VacuumPerception> perceptions = new List<VacuumPerception>();
+
+        public VacuumPerceptionList(params VacuumPerception[] perceptions)
+        {
+            foreach (var perception in perceptions)
+                this.perceptions.Add(perception);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is VacuumPerceptionList)
+            {
+                var list = (VacuumPerceptionList)obj;
+
+                if (this.perceptions.Count != list.perceptions.Count)
+                    return false;
+
+                for (int k = 0; k < this.perceptions.Count; k++)
+                    if (!this.perceptions[k].Equals(list.perceptions[k]))
+                        return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int result = hashcode;
+
+            foreach (var perception in this.perceptions)
+            {
+                result *= 17;
+                result += perception.GetHashCode();
+            }
+
+            return result;
+        }
+    }
+}
