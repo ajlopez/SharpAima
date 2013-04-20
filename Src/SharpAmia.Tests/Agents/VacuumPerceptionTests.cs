2776e8ad32d5f2f983062a62d63575c8fbf34168
ajlopez <webmaster@ajlopez.com>
2013-04-20 17:40:20 -0300

    Vacuum Perception Equals, prepare to Table Driven

---
+++ Src/SharpAmia.Tests/Agents/VacuumPerceptionTests.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpAmia.Tests.Agents
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpAmia.Agents;
+
+    [TestClass]
+    public class VacuumPerceptionTests
+    {
+        [TestMethod]
+        public void Equals()
+        {
+            VacuumPerception p1 = new VacuumPerception(VacuumLocation.A, VacuumStatus.Clean);
+            VacuumPerception p2 = new VacuumPerception(VacuumLocation.B, VacuumStatus.Clean);
+            VacuumPerception p3 = new VacuumPerception(VacuumLocation.A, VacuumStatus.Dirty);
+            VacuumPerception p4 = new VacuumPerception(VacuumLocation.B, VacuumStatus.Dirty);
+            VacuumPerception p5 = new VacuumPerception(VacuumLocation.A, VacuumStatus.Clean);
+
+            Assert.IsTrue(p1.Equals(p1));
+            Assert.IsTrue(p1.Equals(p5));
+            Assert.IsTrue(p5.Equals(p1));
+            Assert.AreEqual(p1.GetHashCode(), p5.GetHashCode());
+
+            Assert.IsFalse(p1.Equals(null));
+            Assert.IsFalse(p1.Equals(123));
+            Assert.IsFalse(p1.Equals(p2));
+            Assert.IsFalse(p2.Equals(p1));
+            Assert.IsFalse(p1.Equals(p3));
+            Assert.IsFalse(p3.Equals(p1));
+            Assert.IsFalse(p1.Equals(p4));
+            Assert.IsFalse(p4.Equals(p1));
+        }
+    }
+}
--- Src/SharpAmia/Agents/VacuumPerception.cs
+++ Src/SharpAmia/Agents/VacuumPerception.cs
@@ -19,5 +19,26 @@
         public VacuumStatus Status { get { return this.status; } }
 
         public VacuumLocation Location { get { return this.location; } }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is VacuumPerception)
+            {
+                var perp = (VacuumPerception)obj;
+
+                if (this.location == perp.location && this.status == perp.status)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.location.GetHashCode() + this.status.GetHashCode();
+        }
     }
 }
